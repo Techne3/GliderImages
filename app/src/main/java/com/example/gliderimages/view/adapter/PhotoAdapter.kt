4bbc43ff79b2b8b4051200c74333917bdd46e337
Timothy Sharkey <Techne3@gmail.com>
2022-02-02 12:59:48 -0500

    Loading images from the API using Glide - Fixed the context in the Glide args

--- app/src/main/java/com/example/gliderimages/view/adapter/PhotoAdapter.kt
+++ app/src/main/java/com/example/gliderimages/view/adapter/PhotoAdapter.kt
@@ -1,47 +1,61 @@
 package com.example.gliderimages.view.adapter
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.GlideContext
+import com.example.gliderimages.R
 import com.example.gliderimages.databinding.PhotoItemsBinding
 import com.example.gliderimages.network.models.PhotoModel
+import kotlin.coroutines.coroutineContext
 
 class PhotoAdapter(
     private val userList: List<PhotoModel>
-    ) : RecyclerView.Adapter<PhotoAdapter.UserViewHolder>() {
-
+) : RecyclerView.Adapter<PhotoAdapter.UserViewHolder>() {
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
-            val inflater = LayoutInflater.from(parent.context)
-            val binding = PhotoItemsBinding.inflate(inflater, parent, false)
-            return UserViewHolder(binding)
-        }
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = PhotoItemsBinding.inflate(inflater, parent, false)
+        return UserViewHolder(binding)
+    }
 
-        override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
-            holder.bind(userList[position])
-        }
-
-        override fun getItemCount(): Int {
-            return userList.size
-        }
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+        holder.bind(userList[position])
 
 
 
-        class UserViewHolder(private val binding: PhotoItemsBinding) :
-            RecyclerView.ViewHolder(binding.root) {
 
 
+    }
 
-            fun bind(photo: PhotoModel) {
-                var url = "https://picsum.photo/80"
-                with(binding) {
-                    albumTv.text = photo.albumId.toString()
-                    titleTv.text = photo.title
-                    urlTv.text = photo.url
-                }
+    override fun getItemCount(): Int {
+        return userList.size
+    }
+
+
+    class UserViewHolder(private val binding: PhotoItemsBinding ) :
+        RecyclerView.ViewHolder(binding.root) {
+
+
+        fun bind(photo: PhotoModel ) {
+            with(binding) {
+                albumTv.text = photo.albumId.toString()
+                titleTv.text = photo.title
+                urlTv.text = photo.url
+
+                val urls = photo.thumbnailUrl
+
+                Glide.with(photoImage.context)
+                    .load("${urls}.png")
+                    .placeholder(R.drawable.ic_launcher_background)
+                    .into(photoImage)
+
+
             }
         }
     }
+}
